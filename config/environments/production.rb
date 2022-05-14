631606d33871677c0e03eb2383f04cb7f9760760
an-lee <an.lee.work@gmail.com>
2022-05-14 20:00:19 +0800

    rename project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "trident_lab_production"
+  # config.active_job.queue_name_prefix = "trident_assistant_web_production"
 
   config.action_mailer.perform_caching = false
 
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,14 +3,14 @@
 require 'sidekiq/throttled'
 
 Sidekiq.configure_server do |config|
-  config.redis = { namespace: 'trident_lab_sidekiq' }
+  config.redis = { namespace: 'trident_assistant_web_sidekiq' }
 
   cron_file = 'config/sidekiq-cron.yml'
   Sidekiq::Cron::Job.load_from_hash YAML.load_file(cron_file) || {} if File.exist?(cron_file) && Sidekiq.server?
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { namespace: 'trident_lab_sidekiq' }
+  config.redis = { namespace: 'trident_assistant_web_sidekiq' }
 end
 
 Sidekiq::Throttled.setup!
