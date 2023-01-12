912221caf62099430c5a4f09dd1f380d1d6827bf
an-lee <an.lee.work@gmail.com>
2023-01-12 15:18:29 +0800

    fix authorize trident

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,7 +10,8 @@ class HomeController < ApplicationController
       current_user
       .mixin_api.authorize_code(
         user_id: '6f5a84ce-d663-451e-b413-2d0c84b7629d',
-        scope: ['PROFILE:READ', 'COLLECTIBLES:RESD']
+        scope: ['PROFILE:READ', 'COLLECTIBLES:RESD'],
+        pin: user.keystore_json['pin']
       )
     redirect_to "https://thetrident.one/auth/mixin/callback?code=#{code}", allow_other_host: true
   end
