8ceae4c96f28295bb12a08f275d28d1d8b175ecb
an-lee <an.lee.work@gmail.com>
2023-01-12 15:12:08 +0800

    may authorize trident

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,14 @@ class HomeController < ApplicationController
   def index
     redirect_to collections_path
   end
+
+  def authorize_trident
+    code =
+      current_user
+      .mixin_api.authorize_code(
+        user_id: '6f5a84ce-d663-451e-b413-2d0c84b7629d',
+        scope: ['PROFILE:READ', 'COLLECTIBLES:RESD']
+      )
+    redirect_to "https://thetrident.one/auth/mixin/callback?code=#{code}", allow_other_host: true
+  end
 end
