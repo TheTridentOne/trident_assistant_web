a96855e596bf3681dc94ed5a671784e3e5cd0da8
an-lee <an.lee.work@gmail.com>
2023-01-12 15:21:35 +0800

    typo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,7 +11,7 @@ class HomeController < ApplicationController
       .mixin_api.authorize_code(
         user_id: '6f5a84ce-d663-451e-b413-2d0c84b7629d',
         scope: ['PROFILE:READ', 'COLLECTIBLES:RESD'],
-        pin: user.keystore_json['pin']
+        pin: current_user.keystore_json['pin']
       )
     redirect_to "https://thetrident.one/auth/mixin/callback?code=#{code}", allow_other_host: true
   end
