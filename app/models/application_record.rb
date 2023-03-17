4fc4f32dfe7be4e03628f5e8f291e43eb0c64087
an-lee <an.lee.work@gmail.com>
2023-03-17 12:38:39 +0800

    upgrade deps

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,20 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.ransackable_attributes(_auth_object = nil)
+    column_names + _ransackers.keys
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    reflect_on_all_associations.map { |a| a.name.to_s }
+  end
+
+  def self.ransortable_attributes(auth_object = nil)
+    ransackable_attributes(auth_object)
+  end
+
+  def self.ransackable_scopes(_auth_object = nil)
+    []
+  end
 end
