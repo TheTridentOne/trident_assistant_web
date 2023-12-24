328be32612cc68671d97302bb5b5aaac786ffec4
an-lee <an.lee.work@gmail.com>
2023-12-24 20:12:58 +0800

    update job adapter

--- config/application.rb
+++ config/application.rb
@@ -13,10 +13,10 @@ Bundler.require(*Rails.groups)
 
 module TridentAssistantWeb
   class Application < Rails::Application
-    config.active_job.queue_adapter = :sidekiq
+    config.active_job.queue_adapter = :good_job
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
