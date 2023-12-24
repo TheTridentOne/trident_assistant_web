761459f883937533d37af1e7b5c0624997855f18
an-lee <an.lee.work@gmail.com>
2023-12-24 20:20:25 +0800

    fix syntax

--- app/jobs/task_process_job.rb
+++ app/jobs/task_process_job.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class TaskProcessJob < ApplicationJob
-  retry_on MixinBot::InsufficientPoolError, MixinBot::PinError, waits: :exponentially_longer, attempts: Float::INFINITY
-
   def perform(id)
     Task.find_by(id: id)&.process!
   end
